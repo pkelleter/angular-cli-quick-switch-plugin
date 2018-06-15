101aca51ba053fdbf1b1a23b44766c30b5b68c4e
CooperWolfe <cooper.wolfe@gmail.com>
2018-06-15 19:16:38 +0200

    added support for php, haml, pug (previously jade), slim, less, and stylus

--- src/main/java/QuickSwitchAction.java
+++ src/main/java/QuickSwitchAction.java
@@ -13,7 +13,11 @@ public class QuickSwitchAction extends AnAction {
 
     private AnActionEvent latestEvent;
 
-    private List<String> extensions = Arrays.asList("ts", "js", "html", "css", "sass", "scss");
+    private List<String> extensions = Arrays.asList(
+      "ts", "js",
+      "html", "php", "haml", "jade", "pug", "slim",
+      "css", "sass", "scss", "less", "styl"
+    );
 
     public QuickSwitchAction() {
         super("QuickSwitch");
